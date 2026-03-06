1db91e80a47d3c09c1e2d344cf0232f55665bc8a
Iryna <irinakobyzhcha16@ukr.net>
2026-03-06 22:08:42 +0200

    add gallery code base

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -5,10 +5,25 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
+import about1 from '../img/about1.jpg';
+import about2 from '../img/about2.jpg';
+import about3 from '../img/about3.jpg';
+import about4 from '../img/about4.jpg';
+import about5 from '../img/about5.jpg';
+
 const aboutSwiperEl = document.querySelector('.js-about-swiper');
 
 if (aboutSwiperEl) {
-  new Swiper(aboutSwiperEl, {
+  const images = aboutSwiperEl.querySelectorAll('.about-card-img');
+  const imgs = [about1, about2, about3, about4, about5];
+
+  images.forEach((img, index) => {
+    if (imgs[index]) {
+      img.src = imgs[index];
+    }
+  });
+
+  new Swiper('.js-about-swiper', {
     modules: [Navigation, Pagination, Keyboard],
     slidesPerView: 1,
     spaceBetween: 24,
