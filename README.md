9e62bdb3bfb9168c8506b3024d235a3e160e9d19
viktorhnatiuk136 <viktorgnatiuk136@gmail.com>
2026-03-08 17:49:32 +0000

    Deploying to gh-pages from @ SCBProd/pets_founder@c310106404631f7e8e1d3da473aa057ff56d33e4 🚀

--- README.md
+++
@@ -1,2 +0,0 @@
-# pets_founder
-pets_founder
--- src/js/faq.js
+++
@@ -1,6 +0,0 @@
-import Accordion from "accordion-js";
-import "accordion-js/dist/accordion.min.css";
-new Accordion(".accordion-container", {
-  duration: 400,
-  showMultiple: false,
-});
